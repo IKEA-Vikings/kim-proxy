d5d82db169005c8757a3318540e3a36f9fad6f09
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2021-03-17 11:51:14 -0500

    config files

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+
+  mode: 'development',
+  entry: './client/index.html',
+  output: {
+    path: path.resolve(__dirname, 'public/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {test: /\.html$/, use: 'html-loader'}
+    ]
+  }
+};
