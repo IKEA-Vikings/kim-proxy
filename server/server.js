222082598c3367fd5c2cd62bc50255ca59429652
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2021-04-10 14:11:12 -0500

    added routes for about and image service to use aws urls instead of localhost

--- server/server.js
+++ server/server.js
@@ -20,11 +20,31 @@ app.get('/:id', (req, res) => {
 });
 
 // NEED FOR VIRGINIA'S SERVICE
-app.get(`/api/sizes/:id`, (req, res) => {
+// app.get(`/api/sizes/:id`, (req, res) => {
+//   console.log('GET SIZES');
+//   axios.get(`http://localhost:3002/api/sizes/${req.params.id}`)
+//       .then((result) => {
+//         res.send(result.data);
+//       })
+//       .catch((err) => console.error('GET PRODUCT SIZES FAILED: ', err));
+// });
+
+// need second routes else my service fails
+app.get(`/api/product/:id`, (req, res) => {
   console.log('GET SIZES');
-  axios.get(`http://localhost:3002/api/sizes/${req.params.id}`)
+  axios.get(`http://ec2-3-86-58-21.compute-1.amazonaws.com:3003/api/product/${req.params.id}`)
       .then((result) => {
         res.send(result.data);
       })
-      .catch((err) => console.error('GET PRODUCT SIZES FAILED: ', err));
+      .catch((err) => console.error('GET PRODUCT ABOUT FAILED: ', err));
+});
+
+// PHUCCI
+app.get(`/images/org/:id`, (req, res) => {
+  console.log('GET IMAGES');
+  axios.get(`http://ec2-54-67-28-46.us-west-1.compute.amazonaws.com:4004/images/org/${req.params.id}`)
+      .then((result) => {
+        res.send(result.data);
+      })
+      .catch((err) => console.error('GET PRODUCT IMAGES FAILED: ', err));
 });
